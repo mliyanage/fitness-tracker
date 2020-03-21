2e47b724aae99d68bfc803901d2e4202e7b5f4ce
mliyanage <mliyanage@gmail.com>
2020-03-21 15:38:00 +0400

    modified the training service

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,46 +1,51 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { StopTrainingComponent } from './stop-training.component';
+import { TrainingService } from "./../training.service";
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { StopTrainingComponent } from "./stop-training.component";
 
 @Component({
-  selector: 'app-current-training',
-  templateUrl: './current-training.component.html',
-  styleUrls: ['./current-training.component.scss']
+  selector: "app-current-training",
+  templateUrl: "./current-training.component.html",
+  styleUrls: ["./current-training.component.scss"]
 })
 export class CurrentTrainingComponent implements OnInit {
-  @Output() trainingExit = new EventEmitter();
-  progress=0;
+  // @Output() trainingExit = new EventEmitter();
+  progress = 0;
   timer: number;
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private trainingService: TrainingService
+  ) {}
 
   ngOnInit(): void {
     this.startOrResumeTimer();
   }
 
-  startOrResumeTimer(){
-    this.timer = setInterval(()=>{
-      this.progress = this.progress + 5;
-      if (this.progress >= 100){
+  startOrResumeTimer() {
+    const step =
+      (this.trainingService.getRunningExercise().duration / 100) * 1000;
+
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 1;
+      if (this.progress >= 100) {
+        this.trainingService.completeSercise();
         clearInterval(this.timer);
       }
-    }, 1000)
+    }, step);
   }
-  onStop(){
+  onStop() {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
-      data:{progress: this.progress}
+      data: { progress: this.progress }
     });
-    
+
     dialogRef.afterClosed().subscribe(result => {
-      if(result) {
-        this.trainingExit.emit();
-      }
-      else{
+      if (result) {
+        // this.trainingExit.emit();
+        this.trainingService.cancelExersise(this.progress);
+      } else {
         this.startOrResumeTimer();
       }
-    })
-
+    });
   }
-
-  
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -11,6 +11,7 @@ export class TrainingService {
   exerciseChanged = new Subject<Exercise>();
 
   private runningExercise: Exercise;
+  private exercises: Exercise[] = [];
 
   getAvailableExercises() {
     return this.availableEcercises.slice();
@@ -23,7 +24,33 @@ export class TrainingService {
     this.exerciseChanged.next({ ...this.runningExercise });
   }
 
+  completeSercise() {
+    this.exercises.push({
+      ...this.runningExercise,
+      date: new Date(),
+      state: "completed"
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
+  cancelExersise(progress: number) {
+    this.exercises.push({
+      ...this.runningExercise,
+      duration: this.runningExercise.duration * (progress / 100),
+      calories: this.runningExercise.duration * (progress / 100),
+      date: new Date(),
+      state: "cancelled"
+    });
+    this.runningExercise = null;
+    this.exerciseChanged.next(null);
+  }
+
   stopExercise() {
     this.exerciseChanged.next(null);
+  }
+
+  getRunningExercise() {
+    return { ...this.runningExercise };
   }
 }

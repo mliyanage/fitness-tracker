d8889566b79dad20e9e175933f73ddbe6b3f4cf9
mliyanage <mliyanage@gmail.com>
2020-03-25 16:22:07 +0400

    added pagination

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -15,6 +15,8 @@ import { MatSelectModule } from "@angular/material/select";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatTableModule } from "@angular/material/table";
+import { MatSortModule } from "@angular/material/sort";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule({
   imports: [
@@ -33,7 +35,9 @@ import { MatTableModule } from "@angular/material/table";
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   exports: [
     MatButtonModule,
@@ -51,7 +55,9 @@ import { MatTableModule } from "@angular/material/table";
     MatSelectModule,
     MatProgressSpinnerModule,
     MatDialogModule,
-    MatTableModule
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ]
 })
 export class MaterialModule {}
